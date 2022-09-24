e815e18c39511652db10169a93a28090fd9d4e03
Chin Wee <chokcw@gmail.com>
2022-09-24 23:25:19 +0800

    Add add and remove book features

--- javascript.js
+++ javascript.js
@@ -13,18 +13,55 @@ function Library() {
     this.books = [];
 }
 
+const myLibrary = new Library();
+
 Library.prototype.addBook = function(book) {
     this.books.push(book);
     console.log("Booked added.");
 }
 
 Library.prototype.displayBooks = function() {
+    bookList.textContent = ''
     for (const book of this.books) {
-
         bookCard = createBookCard(book);
         bookList.appendChild(bookCard);
     }
 }
+
+Library.prototype.removeBook = function(removeTitle) {
+    removeBookPosition = 0
+    for (const book of this.books) {
+        if (book.title === removeTitle) {
+            this.books.splice(removeBookPosition, 1);
+        }
+        else {
+            removeBookPosition++;
+        }
+    }
+}
+
+Library.prototype.saveLocal = function() {
+    localStorage.clear();
+    localStorage.setItem('myLibrary', JSON.stringify(this.books))
+}
+
+Library.prototype.restoreLocal = function() {
+    const books = JSON.parse(localStorage.getItem('myLibrary'))
+    if (books) {
+        this.books = books.map((book) => jsonToBook(book))
+    } else {
+        this.books = []
+    }
+}
+
+function jsonToBook(book) {
+    return new Book(book.title, book.author, book.pages, book.read)
+}
+
+bookList = document.querySelector(".book-list");
+myLibrary.restoreLocal();
+myLibrary.displayBooks();
+
 
 function createBookCard(book) {
     const bookCard = document.createElement("div");
@@ -38,12 +75,22 @@ function createBookCard(book) {
     title.classList.add('book-title');
     remove.classList.add('book-remove');
     read.classList.add('book-read-status');
+   
 
     title.textContent = book["title"]
     author.textContent = book["author"]
     pages.textContent = `${book["pages"]} pages`
     read.textContent = book["read"];
     remove.textContent = "x";
+
+    if (book["read"] === true) {
+        read.textContent = "Read"
+        read.classList.add('book-read-true');
+    }
+    else {
+        read.textContent = "Not read"
+        read.classList.add('book-read-false');
+    }
 
     bookCard.appendChild(title);
     bookCard.appendChild(author);
@@ -58,9 +105,17 @@ function createBookCard(book) {
 // Events to add book
 const addBookButton = document.querySelector(".add-book");
 var addBookModal = document.getElementById("addBookModal"); // Get the modal
-var closeSpan = document.querySelector(".close"); // Get the <span> element that closes the modal
+// var closeSpan = document.querySelector(".close"); // Get the <span> element that closes the modal
+const addBookForm = document.querySelector(".add-book-form");
+addBookForm.library = myLibrary
 
-function addBook(e) {
+const newBookTitle = document.querySelector("#new-book-title");
+const newBookAuthor = document.querySelector("#new-book-author");
+const newBookPages = document.querySelector("#new-book-pages");
+const newBookRead = document.querySelector("#new-book-read");
+
+
+function openAddBookModal(e) {
     // When the user adds book, open the modal
     addBookModal.style.display = "block";
 }
@@ -77,21 +132,47 @@ function closeAddBookModalWindow(e) {
     }
 }
 
-addBookButton.addEventListener('click', addBook);
-closeSpan.addEventListener('click', closeAddBookModalSpan);
+function confirmAddBook(e) {
+    var title = newBookTitle.value;
+    var author = newBookAuthor.value;
+    var pages = newBookPages.value;
+    var read = newBookRead.checked
+
+    newBook = new Book(title, author, pages, read);
+    e.currentTarget.library.addBook(newBook);
+    e.currentTarget.library.saveLocal();
+}
+
+addBookButton.addEventListener('click', openAddBookModal);
+// closeSpan.addEventListener('click', closeAddBookModalSpan);
 window.addEventListener('click', closeAddBookModalWindow);
+addBookForm.addEventListener('submit', confirmAddBook);
+
+
+// Events to remove book
+const removeBookButtons = document.querySelectorAll(".book-remove");
+
+function removeBook(e) {
+    targetParent = e.currentTarget.parentElement;
+    title = targetParent.querySelector(".book-title").textContent;
+
+    e.currentTarget.library.removeBook(title);
+    e.currentTarget.library.saveLocal();
+    e.currentTarget.library.displayBooks();
+}
+
+removeBookButtons.forEach((removeBookButton) => {
+    removeBookButton.addEventListener('click', removeBook);
+    removeBookButton.library = myLibrary;
+})
 
 
 
 
 
-bookList = document.querySelector(".book-list");
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Not read");
-const harryPotter = new Book("Harry Potter", "J.K. Rowling", 295, "Read");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const harryPotter = new Book("Harry Potter", "J.K. Rowling", 295, true);
 
-const myLibrary = new Library();
-
-myLibrary.addBook(theHobbit);
-myLibrary.addBook(harryPotter);
-myLibrary.displayBooks();
+// myLibrary.addBook(theHobbit);
+// myLibrary.addBook(harryPotter);
